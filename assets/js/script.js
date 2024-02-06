87f778735ec1652fa14ce222a2758bdd696dace1
ragranato <robert.granato@gmail.com>
2024-02-05 21:17:40 -0500

    fixed logic to add tasks to page, working on delete button

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,8 +5,10 @@ const dueDate = document.getElementById("due-date");
 const urgency = document.getElementById("urgency");
 const formSubmit = document.getElementById("form-submit");
 const form = document.getElementById("form");
+const deleteButton = document.getElementsByClassName('delete')
 let current = 1;
-const taskArray = [];
+let taskArray = [];
+let tasksHTMLlist = [];
 
 const newTask = () => {
     const cardObj = {
@@ -23,38 +25,41 @@ const newTask = () => {
 const createHTML = (obj) => {
   const { id, name, description, urgency, date } = obj;
   let cardHTML = `   
-        <div class="task-card">
+        <div class="task-card" id="${id}">
             <h3><span>${id}: </span> ${name.toUpperCase()}</h3>
             <p>${description}</p>
             <p class="due">Due: ${date}</p>
-            <h4 class="urgency-text"><strong>${urgency.toUpperCase()}</strong></h4>
-            <div>
-                <button class='delete' >Delete</button>
-                
-            </div>        
+            <h4 class="urgency-text"><strong>${urgency.toUpperCase()}</strong></h4>            
+            <button class='delete'  >Delete</button>                               
         </div>     
     `;
-
   return cardHTML;
 };
 
 
+const render = () => {
+    const tasksHTML = tasksHTMLlist.join("\n");
+    cardSection.innerHTML = tasksHTML;
+}
+
 
 const createCard = (e) => {
   e.preventDefault();   
-  const currentObj = newTask() 
-  const tasksHTMLlist = []
-  
-  taskArray.forEach((task) =>{
-    let html = createHTML(task);
-    tasksHTMLlist.push(html)    
-  })
-  cardSection.innerHTML += tasksHTMLlist.join("\n");
+  const currentObj = newTask()
+  let html = createHTML(taskArray[currentObj.id-1]);
+  tasksHTMLlist.push(html);   
+  render() 
   cardSection.style.display = "flex";
   current++;
+  document.getElementById("form").reset();
 };
 
-const deleteBtn = () => {};
-const editBtn = () => {};
+const deleteBtn = (id) => {
+    
+    
+    render()    
+};
+
 
 formSubmit.addEventListener("click", createCard);
+deleteButton.addEventListener('click', deleteBtn)
